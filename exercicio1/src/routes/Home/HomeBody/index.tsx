f4e7195337b10aa6d890af03385866532e49c6c3
Nelio Alves <acenelio@gmail.com>
2022-11-01 12:32:11 -0300

    Layout

--- exercicio1/src/routes/Home/HomeBody/index.tsx
+++ exercicio1/src/routes/Home/HomeBody/index.tsx
@@ -1,8 +1,20 @@
+import Button from "../../../components/Button";
+import Card from "../../../components/Card";
+
 export default function HomeBody() {
 
     return (
         <main>
-            <h2>Página inicial</h2>
+            <section>
+                <div className="container">
+                    <div className="mt30 mb30">
+                        <Card title="Página inicial" />
+                    </div>
+                    <div className="dflex">
+                        <Button text="Ver promoção" />
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
